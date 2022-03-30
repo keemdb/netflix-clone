7bf807a001bd6d03a2b77b52216d3f08ebe852a0
Dongbeom Kim <keemdb@gmail.com>
2022-03-30 20:42:48 +0900

    add useEffect to Row.js

---
+++ src/Row.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+
+function Row ({ title, fetchUrl }){
+
+	const [movies, setMovie] = useState([]);
+
+	useEffect(()=>{
+		async function fetchData(){
+			const request = await axios.get(fetchUrl);
+			console.log(request);
+			return request;
+		}
+		fetchData();
+	}, []);
+
+
+	return (
+		<div>
+			<h2>{title}</h2>
+			{/* container -> posters */}
+		</div>
+	)
+}
+
+export default Row;
