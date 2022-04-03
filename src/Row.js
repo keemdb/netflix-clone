c4ddd7bc26d5a32db7ad8045afca10252d0a4f1b
Dongbeom Kim <keemdb@gmail.com>
2022-04-03 23:08:03 +0900

    poster 적용

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
+import './Row.css';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row ({ title, fetchUrl }){
 
@@ -15,11 +18,22 @@ function Row ({ title, fetchUrl }){
 		fetchData();
 	}, [fetchUrl]);
 
+	console.log(movies);
 
 	return (
-		<div>
+		<div className="row">
 			<h2>{title}</h2>
-			{/* container -> posters */}
+			<div className="row__posters">
+
+				{movies.map(movie => (
+					<img 
+						key={movie.id}
+						className="row__poster"
+						src={`${base_url}${movie.poster_path}`} 
+						alt={movie.name}
+					/>
+				))}
+			</div>
 		</div>
 	)
 }
